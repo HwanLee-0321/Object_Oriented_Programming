8406fdaf1780bc4303e32494ec455fb5e2c5b519
HwanLee <ghksdl413@naver.com>
2025-06-22 22:20:46 +0900

    CH10_previous_exam2.cpp 파일 추가: 문자열 입력 및 길이 출력 기능 구현

---
+++ CH10/CH10_previous_exam/CH10_previous_exam2.cpp
@@ -0,0 +1,27 @@
+#include <iostream> //공백지우고 활용
+#include <vector> //공백지우고 활용
+#include <algorithm> //공백지우고 활용
+#define MAX_SIZE  5
+using namespace std;
+
+
+int main() {
+
+    vector<string> strVec;
+
+    // 문자열 입력 받기
+    cout << "문자열을 최대 " << MAX_SIZE << "개 입력하세요:" << endl;
+    for (int i = 0; i < MAX_SIZE; ++i) {
+        string str;
+        cout << "문자열 " << i + 1 << ": ";
+        cin >> str;
+        strVec.push_back(str); // vector에 삽입
+    }
+
+    // string 자료형의 문자열 길이를 구하는 함수는 .length() 이다.
+    cout << "각 문자열의 길이:" << endl;
+    for_each(strVec.begin(), strVec.end(), [](const string& str) {
+        cout << str << "의 길이: " << str.length() << endl;
+    });
+    return 0;
+}
