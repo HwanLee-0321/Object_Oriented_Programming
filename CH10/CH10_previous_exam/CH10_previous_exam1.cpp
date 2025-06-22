d148f8efdd0438aa4559ce871c33ae3fed4d868e
HwanLee <ghksdl413@naver.com>
2025-06-22 22:04:55 +0900

    CH10_previous_exam1.cpp 파일 추가: 최소값을 찾는 템플릿 함수 및 사용자 입력 기능 구현

---
+++ CH10/CH10_previous_exam/CH10_previous_exam1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#define MAX_ARRAY_SIZE 6
+using namespace std;
+
+template <class T>
+T smallest(T a[], int n) {
+	T min = a[0];
+	for (int i = 1; i < n; i++) {
+		if (a[i] < min) {
+		min = a[i];
+		}
+	}
+	return min;
+}	
+
+int main() {
+	int x[MAX_ARRAY_SIZE];
+	double y[MAX_ARRAY_SIZE];
+
+	cout << "정수 " << MAX_ARRAY_SIZE << "개를 입력하세요:";
+	for (int i = 0; i < MAX_ARRAY_SIZE; i++)
+		cin >> x[i];
+	cout << "가장 작은 정수는: " << smallest(x, MAX_ARRAY_SIZE) << endl;
+
+	
+	cout << "실수 " << MAX_ARRAY_SIZE << "개를 입력하세요:";
+	
+	for (int i = 0; i < MAX_ARRAY_SIZE; i++)
+		cin >> y[i];
+	cout << "가장 작은 실수는: " << smallest(y, MAX_ARRAY_SIZE) << endl;
+
+	return 0;
+}
